7409a29b2e90c1a6a943cf0a9d163e5a6c971abb
Leonardo Nunes <leonunesbs@gmail.com>
2024-04-08 19:55:29 -0300

    fix: fix

--- src/app/v2/page.tsx
+++ src/app/v2/page.tsx
@@ -1,6 +1,6 @@
-import nodemailer from 'nodemailer';
-import { MailOptions } from 'nodemailer/lib/json-transport';
+import { redirect } from 'next/navigation';
 import { PDFDocument, StandardFonts } from 'pdf-lib';
+import { Resend } from 'resend';
 
 import { Data } from '../page';
 
@@ -95,31 +95,48 @@ export default function V2() {
 
       const pdfBytes = await pdfDoc.save();
 
-      const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true, // Use `true` for port 465, `false` for all other ports
-        auth: {
-          user: process.env.SMTP_USER,
-          pass: process.env.SMTP_PASS,
-        },
-      });
+      const resend = new Resend(process.env.RESEND_API_KEY);
 
-      const mailOptions: MailOptions = {
-        from: 'oftleonunesbs@gmail.com',
-        to: emailTo,
+      const { data } = await resend.emails.send({
+        from: 'Acme <onboarding@resend.dev>',
+        to: [emailTo],
         subject: 'Relatório de Injeções',
         text: 'Segue anexo relatório de injeções.',
         attachments: [
           {
             filename: 'relatorio.pdf',
             content: Buffer.from(pdfBytes),
-            contentType: 'application/pdf',
           },
         ],
-      };
+      });
 
-      transporter.sendMail(mailOptions);
+      redirect(`?id=${data?.id}`);
+
+      // const transporter = nodemailer.createTransport({
+      //   host: 'smtp.gmail.com',
+      //   port: 465,
+      //   secure: true, // Use `true` for port 465, `false` for all other ports
+      //   auth: {
+      //     user: process.env.SMTP_USER,
+      //     pass: process.env.SMTP_PASS,
+      //   },
+      // });
+
+      // const mailOptions: MailOptions = {
+      //   from: 'oftleonunesbs@gmail.com',
+      //   to: emailTo,
+      //   subject: 'Relatório de Injeções',
+      //   text: 'Segue anexo relatório de injeções.',
+      //   attachments: [
+      //     {
+      //       filename: 'relatorio.pdf',
+      //       content: Buffer.from(pdfBytes),
+      //       contentType: 'application/pdf',
+      //     },
+      //   ],
+      // };
+
+      // transporter.sendMail(mailOptions);
     };
     fillPDF();
   }
