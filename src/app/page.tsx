810c39d157c7bb0e1a603e8f350f31e7de6a3a80
Leonardo Nunes <leonunesbs@gmail.com>
2024-04-18 22:09:24 -0300

    feat: client side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import { randomInt } from 'crypto';
-import { put } from '@vercel/blob';
-import { redirect } from 'next/navigation';
+'use client';
+
+import { useRouter } from 'next/navigation';
 import { PDFDocument, StandardFonts } from 'pdf-lib';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 import { ProcessButton } from '@/components';
 
@@ -13,33 +14,75 @@ export interface Data {
   treatmentType: string;
 }
 
+type Inputs = {
+  csvData: string;
+};
+
 export default function V2() {
-  async function action(data: FormData) {
-    'use server';
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<Inputs>();
+
+  function csvToArray(csvText: string): string[][] {
+    // Dividir o texto do CSV em linhas
+    const lines: string[] = csvText.split('\n');
+
+    // Array para armazenar as colunas
+    const result: string[][] = [];
+
+    // Iterar sobre as linhas
+    lines.forEach((line) => {
+      // Dividir a linha em colunas
+      const columns: string[] = line.split(';');
+
+      // Adicionar as colunas ao resultado
+      result.push(columns);
+    });
+
+    return result;
+  }
+
+  const fillPdfTemplateWithData = async (
+    { patientId, patientName, staffName, procedureDate, treatmentType }: Data,
+    modelPDFBytes: ArrayBuffer
+  ) => {
+    const pdfDoc = await PDFDocument.load(modelPDFBytes);
+    const pages = pdfDoc.getPages();
+    const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+
+    pages[0].setFontSize(10);
+    pages[0].setFont(timesRomanFont);
+
+    pages[1].setFontSize(10);
+    pages[1].setFont(timesRomanFont);
+
+    pages[2].setFontSize(10);
+    pages[2].setFont(timesRomanFont);
+
+    // página 1
+    pages[0].drawText(patientName.toUpperCase(), { x: 100, y: 633 });
+    pages[0].drawText(patientId, { x: 475, y: 633 });
+    pages[0].drawText(procedureDate.split(' ')[0], { x: 53, y: 483 });
+    pages[0].drawText(procedureDate.split(' ')[0], { x: 215, y: 483 });
+    pages[0].drawText(procedureDate.split(' ')[0], { x: 60, y: 110 });
+    pages[0].drawText(treatmentType, { x: 50, y: 300 });
+
+    // página 2
+    pages[1].drawText(patientName.toUpperCase(), { x: 100, y: 705 });
+    pages[1].drawText(patientId, { x: 440, y: 705 });
+    pages[1].drawText(procedureDate.split(' ')[0], { x: 45, y: 670 });
+    pages[1].drawText(staffName.toUpperCase(), { x: 75, y: 640 });
+
+    // página 3
+    pages[2].drawText(patientName.toUpperCase(), { x: 75, y: 330 });
+    pages[2].drawText(procedureDate.split(' ')[0], { x: 345, y: 330 });
+    pages[2].drawText(patientName.toUpperCase(), { x: 485, y: 330 });
+    pages[2].drawText(procedureDate.split(' ')[0], { x: 760, y: 330 });
+
+    return await pdfDoc.save();
+  };
+
+  const onSubmit: SubmitHandler<Inputs> = async ({ csvData }) => {
     const modelPDFBytes = await fetch('https://hgf-solutions.vercel.app/modelo.pdf').then((res) => res.arrayBuffer());
-    const csvData = data.get('csvData')?.toString();
-    if (!csvData) {
-      return;
-    }
-
-    function csvToArray(csvText: string): string[][] {
-      // Dividir o texto do CSV em linhas
-      const lines: string[] = csvText.split('\n');
-
-      // Array para armazenar as colunas
-      const result: string[][] = [];
-
-      // Iterar sobre as linhas
-      lines.forEach((line) => {
-        // Dividir a linha em colunas
-        const columns: string[] = line.split(';');
-
-        // Adicionar as colunas ao resultado
-        result.push(columns);
-      });
-
-      return result;
-    }
 
     const processedData = csvToArray(csvData)
       .slice(4)
@@ -51,66 +94,23 @@ export default function V2() {
         treatmentType: row[10],
       }));
 
-    const fillPdfTemplateWithData = async (
-      { patientId, patientName, staffName, procedureDate, treatmentType }: Data,
-      modelPDFBytes: ArrayBuffer
-    ) => {
-      const pdfDoc = await PDFDocument.load(modelPDFBytes);
-      const pages = pdfDoc.getPages();
-      const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+    const pdfDoc = await PDFDocument.create();
+    for (let i = 0; i < processedData.length; i++) {
+      const data = processedData[i];
+      if (!data.patientName) continue;
+      const newPdfBytes = await fillPdfTemplateWithData(data, modelPDFBytes);
+      const newPdfDoc = await PDFDocument.load(newPdfBytes);
+      const copiedPages = await pdfDoc.copyPages(newPdfDoc, [0, 1, 2]);
+      copiedPages.forEach((page) => pdfDoc.addPage(page));
+    }
 
-      pages[0].setFontSize(10);
-      pages[0].setFont(timesRomanFont);
+    const pdfBytes = await pdfDoc.save();
 
-      pages[1].setFontSize(10);
-      pages[1].setFont(timesRomanFont);
+    const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+    const url = window.URL.createObjectURL(blob);
+    router.push(url);
+  };
 
-      pages[2].setFontSize(10);
-      pages[2].setFont(timesRomanFont);
-
-      // página 1
-      pages[0].drawText(patientName.toUpperCase(), { x: 100, y: 633 });
-      pages[0].drawText(patientId, { x: 475, y: 633 });
-      pages[0].drawText(procedureDate.split(' ')[0], { x: 53, y: 483 });
-      pages[0].drawText(procedureDate.split(' ')[0], { x: 215, y: 483 });
-      pages[0].drawText(procedureDate.split(' ')[0], { x: 60, y: 110 });
-      pages[0].drawText(treatmentType, { x: 50, y: 300 });
-
-      // página 2
-      pages[1].drawText(patientName.toUpperCase(), { x: 100, y: 705 });
-      pages[1].drawText(patientId, { x: 440, y: 705 });
-      pages[1].drawText(procedureDate.split(' ')[0], { x: 45, y: 670 });
-      pages[1].drawText(staffName.toUpperCase(), { x: 75, y: 640 });
-
-      // página 3
-      pages[2].drawText(patientName.toUpperCase(), { x: 75, y: 330 });
-      pages[2].drawText(procedureDate.split(' ')[0], { x: 345, y: 330 });
-      pages[2].drawText(patientName.toUpperCase(), { x: 485, y: 330 });
-      pages[2].drawText(procedureDate.split(' ')[0], { x: 760, y: 330 });
-
-      return await pdfDoc.save();
-    };
-    const fillPDF = async () => {
-      const pdfDoc = await PDFDocument.create();
-      for (let i = 0; i < processedData.length; i++) {
-        const data = processedData[i];
-        if (!data.patientName) continue;
-        const newPdfBytes = await fillPdfTemplateWithData(data, modelPDFBytes);
-        const newPdfDoc = await PDFDocument.load(newPdfBytes);
-        const copiedPages = await pdfDoc.copyPages(newPdfDoc, [0, 1, 2]);
-        copiedPages.forEach((page) => pdfDoc.addPage(page));
-      }
-
-      const pdfBytes = await pdfDoc.save();
-
-      const blob = new Blob([pdfBytes], { type: 'application/pdf' });
-      const { url } = await put(`${randomInt(1000, 9999)}.pdf`, blob, {
-        access: 'public',
-      });
-      return url;
-    };
-    redirect(await fillPDF());
-  }
   return (
     <main className="py-10 px-4 space-y-10 w-full min-h-full">
       <section>
@@ -118,12 +118,12 @@ export default function V2() {
           <h1 className="text-2xl mb-6 font-bold">HGF</h1>
           <div className="w-full rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <form action={action} className="space-y-4">
+              <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
                 <div className="form-control">
                   <label htmlFor="csvData" className="label label-text">
                     Dados CSV:
                   </label>
-                  <textarea name="csvData" id="csvData" className="textarea textarea-bordered w-full" required />
+                  <textarea {...register('csvData')} className="textarea textarea-bordered w-full" required />
                 </div>
                 <ProcessButton />
               </form>
