390f979c97dec796b2a248acf610689a341ae075
Leonardo Nunes <leonunesbs@gmail.com>
2024-04-21 17:49:10 -0300

    feat: upgrade seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { Metadata, Viewport } from 'next';
 
 export const metadata: Metadata = {
   title: `Vítreo - Relatório de Injeções`,
-  description: `Processa planilhas e gera relatórios.`,
   robots: {
     index: false,
     follow: true,
@@ -19,25 +18,42 @@ export const metadata: Metadata = {
   publisher: 'Leonardo Nunes',
   openGraph: {
     title: 'Vítreo Injeções',
-    description: 'The React Framework for the Web',
+    description: `Processa planilhas e gera relatórios.`,
     url: 'https://vitreo.in',
     siteName: `Vítreo - Relatório de Injeções`,
     images: [
       {
-        url: 'https://vitreo.in/next.svg',
-        width: 800,
-        height: 600,
-      },
-      {
-        url: 'https://vitreo.in/next.svg', // Must be an absolute URL
-        width: 1800,
-        height: 1600,
-        alt: 'My custom alt',
+        url: 'https://vitreo.in/logo.jpg',
+        width: 400,
+        height: 400,
       },
     ],
     locale: 'pt_BR',
     type: 'website',
   },
+  icons: {
+    icon: [
+      'favicon.ico',
+      {
+        url: '/facicon-32x32.png',
+        sizes: '32x32',
+      },
+      {
+        url: '/facicon-16x16.png',
+        sizes: '16x16',
+      },
+    ],
+    apple: {
+      url: '/apple-touch-icon.png',
+      sizes: '180x180',
+    },
+    other: {
+      rel: 'mask-icon',
+      url: '/safari-pinned-tab.svg',
+      color: '#5773ff',
+    },
+  },
+  manifest: '/site.webmanifest',
 };
 
 export const viewport: Viewport = {
@@ -45,6 +61,8 @@ export const viewport: Viewport = {
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
+  themeColor: '#fff',
+
   // Also supported by less commonly used
   // interactiveWidget: 'resizes-visual',
 };
