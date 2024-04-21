ba69f24a107b8e1db34affa8489c0acc88fe34af
Leonardo Nunes <leonunesbs@gmail.com>
2024-04-21 13:08:28 -0300

    feat: add miscelanious

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,60 @@
 import '@/styles/globals.css';
 
-import { Metadata } from 'next';
+import { Metadata, Viewport } from 'next';
 
 export const metadata: Metadata = {
-  title: `HGF - Relatórios de Injeções`,
+  title: `Vítreo Injeções - Relatórios`,
   description: `Processa planilhas e gera relatórios.`,
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  generator: 'Next.js',
+  applicationName: 'Vítreo Injeções',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Next.js', 'React', 'JavaScript', 'HGF', 'njeções intravítreas'],
+  authors: [{ name: 'Leonardo Nunes', url: 'https://github.com/leonunesbs' }],
+  creator: 'Leonardo Nunes',
+  publisher: 'Leonardo Nunes',
+  openGraph: {
+    title: 'Vítreo Injeções',
+    description: 'The React Framework for the Web',
+    url: 'https://vitreo.in',
+    siteName: 'Vítreo Injeções',
+    images: [
+      {
+        url: 'https://vitreo.in/next.svg',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://vitreo.in/next.svg', // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
+    locale: 'pt_BR',
+    type: 'website',
+  },
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  // Also supported by less commonly used
+  // interactiveWidget: 'resizes-visual',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
