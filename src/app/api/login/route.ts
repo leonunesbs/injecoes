b72281430da639e4222941b6f94d01d2ec9d169f
Leonardo Nunes <leonunesbs@gmail.com>
2024-11-12 20:44:12 -0300

    fix: fix

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -13,8 +13,11 @@ export async function POST(request: Request) {
 
     // Define o cookie 'auth' com a senha e expiração de 7 dias
     cookieStore.set('auth', password, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
       path: '/',
       maxAge: 7 * 24 * 60 * 60, // 7 dias em segundos
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 dias em milissegundos
     });
 
     return NextResponse.json({ success: true });
