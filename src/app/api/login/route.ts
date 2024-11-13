ef5e130b61f50fe627767c3da4a9bfc68df27ea9
Leonardo Nunes <leonunesbs@gmail.com>
2024-11-12 21:00:17 -0300

    fix: fix

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,5 +1,3 @@
-// src/app/api/login/route.ts
-
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
@@ -8,18 +6,13 @@ export async function POST(request: Request) {
   const secretPassword = process.env.SECRET_PASSWORD;
 
   if (password === secretPassword) {
-    // Obtém o gerenciador de cookies
     const cookieStore = cookies();
-
-    // Define o cookie 'auth' com a senha e expiração de 7 dias
     cookieStore.set('auth', password, {
-      secure: process.env.NODE_ENV === 'production',
+      httpOnly: true,
       path: '/',
       maxAge: 7 * 24 * 60 * 60, // 7 dias em segundos
-      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 dias em milissegundos
       sameSite: 'none',
     });
-
     return NextResponse.json({ success: true });
   } else {
     return NextResponse.json({ success: false }, { status: 401 });
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,3 @@
-// middleware.ts
-
 import { NextResponse } from 'next/server';
 
 import type { NextRequest } from 'next/server';
@@ -7,23 +5,19 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const authCookie = request.cookies.get('auth')?.value;
 
-  // Se o usuário não estiver autenticado e tentar acessar páginas protegidas
   if (!authCookie && request.nextUrl.pathname === '/') {
     const loginUrl = new URL('/login', request.url);
     return NextResponse.redirect(loginUrl);
   }
 
-  // Se o usuário estiver autenticado e tentar acessar a página de login
   if (authCookie && request.nextUrl.pathname === '/login') {
     const dashboardUrl = new URL('/', request.url);
     return NextResponse.redirect(dashboardUrl);
   }
 
-  // Permite que a requisição continue para outras rotas
   return NextResponse.next();
 }
 
-// Configuração do matcher para aplicar o middleware apenas em rotas específicas
 export const config = {
   matcher: ['/', '/login'],
 };
