a401ac150f04d2967a81b843af18702043e73751
Leonardo Nunes <leonunesbs@gmail.com>
2024-11-12 23:28:37 -0300

    fix: fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,21 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const authCookie = request.cookies.get('auth')?.value;
 
+  if (authCookie !== process.env.SECRET_PASSWORD) {
+    // Remove o cookie 'auth' definindo-o com uma data de expiração no passado e redireciona para a página de login
+
+    const loginUrl = new URL('/login', request.url);
+    const response = NextResponse.redirect(loginUrl);
+
+    response.cookies.delete({
+      name: 'auth',
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      path: '/',
+      maxAge: 0,
+    });
+  }
+
   if (!authCookie && request.nextUrl.pathname === '/') {
     const loginUrl = new URL('/login', request.url);
     return NextResponse.redirect(loginUrl);
