84f1e66b1fa870fde764d43880badec2fc11ce8d
Leonardo Nunes <leonunesbs@gmail.com>
2024-11-12 22:08:34 -0300

    fix: fix

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -13,7 +13,6 @@ export async function POST(request: Request) {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       path: '/',
-      maxAge: 7 * 24 * 60 * 60, // 7 dias em segundos
       sameSite: 'none',
     });
 
